dd2b890c460c08d214e6e7baf423aad5d55ba38d
VitorSilvaSantos13 <vitor.slv.snts@hotmail.com>
2020-09-15 20:06:27 -0300

    Backend com SelectAll e SelectDetail

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const UsuarioController = require ('../controllers/usuarioController.js');
 const router = express.Router();
  
+ 
 router.post('/usuarios', UsuarioController.Insert);
+router.get('/usuarios', UsuarioController.SelectAll);
+router.get('/usuarios/:id', UsuarioController.SelectDetail);
+
 
 module.exports = router;
